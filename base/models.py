1400317d7bb58586b0453b6c187013685329ab04
HayleeHauser <hayleebhauser@gmail.com>
2025-02-22 21:57:47 -0500

    3:43:00 - User Profile Page

--- base/models.py
+++ base/models.py
@@ -21,7 +21,6 @@ class Room(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
-
     class Meta:
         ordering = ['-updated', '-created']
 
@@ -35,6 +34,9 @@ class Message(models.Model):
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         return self.body[0:50]
